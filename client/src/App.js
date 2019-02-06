82f19ff2a3ff9c1b0f026642d83865122c44bfb3
jay <jaydanasara@yahoo.com>
2019-02-05 20:54:46 -0500

    first

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Scores from "./pages/Scores";
+import Nav from "./components/Nav";
+
+function App() {
+  return (
+    <div>
+      <Nav />
+      <Scores />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/utils/API.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+export default {
+  // Gets all books
+  getScores: function() {
+    return axios.get("/api/scores");
+  },
+  // Gets the book with the given id
+  getScore: function(id) {
+    return axios.get("/api/scores/" + id);
+  },
+  // Deletes the book with the given id
+  deleteScore: function(id) {
+    return axios.delete("/api/scores/" + id);
+  },
+  // Saves a book to the database
+  saveScore: function(scoreData) {
+    return axios.post("/api/scores", scoreData);
+  }
+};
